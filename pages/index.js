ba63938cbe4555cd4fed98bbefea417cf2605278
Ahkal <bp@sojourn.llc>
2022-08-22 19:26:17 -0400

    - Removes zustand

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,6 @@ export default function Home() {
         />
 
         <CounterCTXUseReducer title="Reducers" description="C2." />
-        <EcommerceZustand />
       </div>
       <Footer />
     </div>
